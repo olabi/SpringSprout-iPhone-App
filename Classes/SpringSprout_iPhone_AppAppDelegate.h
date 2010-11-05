e99f82cf6c4bdced64276cb0f5a7083552288ba8
arawn <arawn.kr@gmail.com>
2010-11-06 01:35:12 +0900

    봄싹 아이폰 앱, 프로젝트 생성

---
+++ Classes/SpringSprout_iPhone_AppAppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  SpringSprout_iPhone_AppAppDelegate.h
+//  SpringSprout-iPhone-App
+//
+//  Created by arawn on 10. 11. 6..
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface SpringSprout_iPhone_AppAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
